9a573b344af6d41698e95cc3a7d0ecd4b37f24c2
Manuele23 <s319694@studenti.polito.it>
2025-03-22 17:38:16 +0100

    Lineups analysis

---
+++ Lineups/lineupsDict.py
@@ -0,0 +1,346 @@
+# Description: This file contains the lineups dictionary that is used to generate the lineups
+
+# ROLE LEGEND (based on StatsBomb terminology):
+
+# Goalkeeper [GK]
+# Center Back [CB]
+# Left Center Back [LCB]
+# Right Center Back [RCB]
+# Left Back [LB]
+# Right Back [RB]
+# Left Wing Back [LWB]
+# Right Wing Back [RWB]
+# Center Defensive Midfield [CDM]
+# Left Defensive Midfield [LDM]
+# Right Defensive Midfield [RDM]
+# Center Midfield [CM]
+# Left Center Midfield [LCM]
+# Right Center Midfield [RCM]
+# Center Attacking Midfield [CAM]
+# Left Attacking Midfield [LAM]
+# Right Attacking Midfield [RAM]
+# Left Midfield [LM]
+# Right Midfield [RM]
+# Left Wing [LW]
+# Right Wing [RW]
+# Center Forward [CF]
+# Left Center Forward [LCF]
+# Right Center Forward [RCF]
+# Secondary Striker [SS]
+
+
+formations = {
+    "3142": [
+        {"role": "Goalkeeper", "x": 5, "y": 40},
+        {"role": "Left Center Back", "x": 20, "y": 20},
+        {"role": "Center Back", "x": 20, "y": 40},
+        {"role": "Right Center Back", "x": 20, "y": 60},
+        {"role": "Center Defensive Midfield", "x": 32, "y": 40},
+        {"role": "Left Midfield", "x": 40, "y": 15},
+        {"role": "Left Center Midfield", "x": 40, "y": 30},
+        {"role": "Right Center Midfield", "x": 40, "y": 50},
+        {"role": "Right Midfield", "x": 40, "y": 65},
+        {"role": "Left Center Forward", "x": 60, "y": 35},
+        {"role": "Right Center Forward", "x": 60, "y": 45}
+    ],
+    "32221": [
+        {"role": "Goalkeeper", "x": 5, "y": 40},
+        {"role": "Left Center Back", "x": 20, "y": 25},
+        {"role": "Center Back", "x": 20, "y": 40},
+        {"role": "Right Center Back", "x": 20, "y": 55},
+        {"role": "Left Wing Back", "x": 30, "y": 15},
+        {"role": "Right Wing Back", "x": 30, "y": 65},
+        {"role": "Left Center Midfield", "x": 38, "y": 30},
+        {"role": "Right Center Midfield", "x": 38, "y": 50},
+        {"role": "Left Wing", "x": 50, "y": 20},
+        {"role": "Right Wing", "x": 50, "y": 60},
+        {"role": "Center Forward", "x": 60, "y": 40}
+    ],
+    "3232": [
+        {"role": "Goalkeeper", "x": 5, "y": 40},
+        {"role": "Left Center Back", "x": 20, "y": 25},
+        {"role": "Center Back", "x": 20, "y": 40},
+        {"role": "Right Center Back", "x": 20, "y": 55},
+        {"role": "Left Wing Back", "x": 30, "y": 15},
+        {"role": "Right Wing Back", "x": 30, "y": 65},
+        {"role": "Left Center Midfield", "x": 40, "y": 30},
+        {"role": "Center Midfield", "x": 40, "y": 40},
+        {"role": "Right Center Midfield", "x": 40, "y": 50},
+        {"role": "Left Center Forward", "x": 60, "y": 35},
+        {"role": "Right Center Forward", "x": 60, "y": 45}
+    ],
+    "3412": [
+        {"role": "Goalkeeper", "x": 5, "y": 40},
+        {"role": "Left Center Back", "x": 20, "y": 20},
+        {"role": "Center Back", "x": 20, "y": 40},
+        {"role": "Right Center Back", "x": 20, "y": 60},
+        {"role": "Left Wing Back", "x": 30, "y": 15},
+        {"role": "Right Wing Back", "x": 30, "y": 65},
+        {"role": "Left Defensive Midfield", "x": 35, "y": 30},
+        {"role": "Right Defensive Midfield", "x": 35, "y": 50},
+        {"role": "Center Attacking Midfield", "x": 45, "y": 40},
+        {"role": "Left Center Forward", "x": 60, "y": 35},
+        {"role": "Right Center Forward", "x": 60, "y": 45}
+    ],
+    "3421": [
+        {"role": "Goalkeeper", "x": 5, "y": 40},
+        {"role": "Left Center Back", "x": 20, "y": 20},
+        {"role": "Center Back", "x": 20, "y": 40},
+        {"role": "Right Center Back", "x": 20, "y": 60},
+        {"role": "Left Wing Back", "x": 30, "y": 15},
+        {"role": "Right Wing Back", "x": 30, "y": 65},
+        {"role": "Left Defensive Midfield", "x": 35, "y": 30},
+        {"role": "Right Defensive Midfield", "x": 35, "y": 50},
+        {"role": "Left Attacking Midfield", "x": 45, "y": 30},
+        {"role": "Right Attacking Midfield", "x": 45, "y": 50},
+        {"role": "Center Forward", "x": 60, "y": 40}
+    ],
+    "343": [
+        {"role": "Goalkeeper", "x": 5, "y": 40},
+        {"role": "Left Center Back", "x": 20, "y": 20},
+        {"role": "Center Back", "x": 20, "y": 40},
+        {"role": "Right Center Back", "x": 20, "y": 60},
+        {"role": "Left Wing Back", "x": 30, "y": 15},
+        {"role": "Right Wing Back", "x": 30, "y": 65},
+        {"role": "Left Defensive Midfield", "x": 35, "y": 30},
+        {"role": "Right Defensive Midfield", "x": 35, "y": 50},
+        {"role": "Left Wing", "x": 50, "y": 20},
+        {"role": "Right Wing", "x": 50, "y": 60},
+        {"role": "Center Forward", "x": 60, "y": 40}
+    ],
+    "3511": [
+        {"role": "Goalkeeper", "x": 5, "y": 40},
+        {"role": "Left Center Back", "x": 20, "y": 20},
+        {"role": "Center Back", "x": 20, "y": 40},
+        {"role": "Right Center Back", "x": 20, "y": 60},
+        {"role": "Left Wing Back", "x": 30, "y": 15},
+        {"role": "Right Wing Back", "x": 30, "y": 65},
+        {"role": "Left Center Midfield", "x": 38, "y": 30},
+        {"role": "Center Defensive Midfield", "x": 38, "y": 40},
+        {"role": "Right Center Midfield", "x": 38, "y": 50},
+        {"role": "Center Attacking Midfield", "x": 50, "y": 40},
+        {"role": "Center Forward", "x": 60, "y": 40}
+    ],
+    "352": [
+        {"role": "Goalkeeper", "x": 5, "y": 40},
+        {"role": "Left Center Back", "x": 20, "y": 25},
+        {"role": "Center Back", "x": 20, "y": 40},
+        {"role": "Right Center Back", "x": 20, "y": 55},
+        {"role": "Left Wing Back", "x": 30, "y": 15},
+        {"role": "Right Wing Back", "x": 30, "y": 65},
+        {"role": "Left Center Midfield", "x": 40, "y": 30},
+        {"role": "Center Defensive Midfield", "x": 40, "y": 40},
+        {"role": "Right Center Midfield", "x": 40, "y": 50},
+        {"role": "Left Center Forward", "x": 60, "y": 35},
+        {"role": "Right Center Forward", "x": 60, "y": 45}
+    ],
+    "41212": [
+        {"role": "Goalkeeper", "x": 5, "y": 40},
+        {"role": "Left Back", "x": 20, "y": 15},
+        {"role": "Left Center Back", "x": 20, "y": 30},
+        {"role": "Right Center Back", "x": 20, "y": 50},
+        {"role": "Right Back", "x": 20, "y": 65},
+        {"role": "Center Defensive Midfield", "x": 32, "y": 40},
+        {"role": "Left Center Midfield", "x": 40, "y": 30},
+        {"role": "Right Center Midfield", "x": 40, "y": 50},
+        {"role": "Center Attacking Midfield", "x": 50, "y": 40},
+        {"role": "Left Center Forward", "x": 60, "y": 35},
+        {"role": "Right Center Forward", "x": 60, "y": 45}
+    ],
+    "41221": [
+        {"role": "Goalkeeper", "x": 5, "y": 40},
+        {"role": "Left Back", "x": 20, "y": 15},
+        {"role": "Left Center Back", "x": 20, "y": 30},
+        {"role": "Right Center Back", "x": 20, "y": 50},
+        {"role": "Right Back", "x": 20, "y": 65},
+        {"role": "Center Defensive Midfield", "x": 32, "y": 40},
+        {"role": "Left Center Midfield", "x": 40, "y": 30},
+        {"role": "Right Center Midfield", "x": 40, "y": 50},
+        {"role": "Left Wing", "x": 50, "y": 20},
+        {"role": "Right Wing", "x": 50, "y": 60},
+        {"role": "Center Forward", "x": 60, "y": 40}
+    ],
+    "4141": [
+        {"role": "Goalkeeper", "x": 5, "y": 40},
+        {"role": "Left Back", "x": 20, "y": 15},
+        {"role": "Left Center Back", "x": 20, "y": 30},
+        {"role": "Right Center Back", "x": 20, "y": 50},
+        {"role": "Right Back", "x": 20, "y": 65},
+        {"role": "Center Defensive Midfield", "x": 32, "y": 40},
+        {"role": "Left Midfield", "x": 40, "y": 15},
+        {"role": "Left Center Midfield", "x": 40, "y": 30},
+        {"role": "Right Center Midfield", "x": 40, "y": 50},
+        {"role": "Right Midfield", "x": 40, "y": 65},
+        {"role": "Center Forward", "x": 60, "y": 40}
+    ],
+    "42121": [
+        {"role": "Goalkeeper", "x": 5, "y": 40},
+        {"role": "Left Back", "x": 20, "y": 15},
+        {"role": "Left Center Back", "x": 20, "y": 30},
+        {"role": "Right Center Back", "x": 20, "y": 50},
+        {"role": "Right Back", "x": 20, "y": 65},
+        {"role": "Left Defensive Midfield", "x": 32, "y": 30},
+        {"role": "Right Defensive Midfield", "x": 32, "y": 50},
+        {"role": "Center Midfield", "x": 40, "y": 40},
+        {"role": "Left Wing", "x": 50, "y": 20},
+        {"role": "Right Wing", "x": 50, "y": 60},
+        {"role": "Center Forward", "x": 60, "y": 40}
+    ],
+    "42211": [
+        {"role": "Goalkeeper", "x": 5, "y": 40},
+        {"role": "Left Back", "x": 20, "y": 15},
+        {"role": "Left Center Back", "x": 20, "y": 30},
+        {"role": "Right Center Back", "x": 20, "y": 50},
+        {"role": "Right Back", "x": 20, "y": 65},
+        {"role": "Left Defensive Midfield", "x": 32, "y": 30},
+        {"role": "Right Defensive Midfield", "x": 32, "y": 50},
+        {"role": "Left Midfield", "x": 40, "y": 15},
+        {"role": "Right Midfield", "x": 40, "y": 65},
+        {"role": "Center Attacking Midfield", "x": 50, "y": 40},
+        {"role": "Center Forward", "x": 60, "y": 40}
+    ],
+    "4222": [
+        {"role": "Goalkeeper", "x": 5, "y": 40},
+        {"role": "Left Back", "x": 20, "y": 15},
+        {"role": "Left Center Back", "x": 20, "y": 30},
+        {"role": "Right Center Back", "x": 20, "y": 50},
+        {"role": "Right Back", "x": 20, "y": 65},
+        {"role": "Left Defensive Midfield", "x": 32, "y": 30},
+        {"role": "Right Defensive Midfield", "x": 32, "y": 50},
+        {"role": "Left Wing", "x": 45, "y": 20},
+        {"role": "Right Wing", "x": 45, "y": 60},
+        {"role": "Left Center Forward", "x": 60, "y": 35},
+        {"role": "Right Center Forward", "x": 60, "y": 45}
+    ],
+    "4231": [
+        {"role": "Goalkeeper", "x": 5, "y": 40},
+        {"role": "Left Back", "x": 20, "y": 15},
+        {"role": "Left Center Back", "x": 20, "y": 30},
+        {"role": "Right Center Back", "x": 20, "y": 50},
+        {"role": "Right Back", "x": 20, "y": 65},
+        {"role": "Left Defensive Midfield", "x": 32, "y": 30},
+        {"role": "Right Defensive Midfield", "x": 32, "y": 50},
+        {"role": "Left Attacking Midfield", "x": 45, "y": 25},
+        {"role": "Center Attacking Midfield", "x": 45, "y": 40},
+        {"role": "Right Attacking Midfield", "x": 45, "y": 55},
+        {"role": "Center Forward", "x": 60, "y": 40}
+    ],
+    "4312": [
+        {"role": "Goalkeeper", "x": 5, "y": 40},
+        {"role": "Left Back", "x": 20, "y": 15},
+        {"role": "Left Center Back", "x": 20, "y": 30},
+        {"role": "Right Center Back", "x": 20, "y": 50},
+        {"role": "Right Back", "x": 20, "y": 65},
+        {"role": "Left Defensive Midfield", "x": 32, "y": 30},
+        {"role": "Right Defensive Midfield", "x": 32, "y": 50},
+        {"role": "Center Attacking Midfield", "x": 45, "y": 40},
+        {"role": "Left Center Forward", "x": 60, "y": 35},
+        {"role": "Right Center Forward", "x": 60, "y": 45},
+        {"role": "Center Forward", "x": 65, "y": 40}
+    ],
+    "4321": [
+        {"role": "Goalkeeper", "x": 5, "y": 40},
+        {"role": "Left Back", "x": 20, "y": 15},
+        {"role": "Left Center Back", "x": 20, "y": 30},
+        {"role": "Right Center Back", "x": 20, "y": 50},
+        {"role": "Right Back", "x": 20, "y": 65},
+        {"role": "Center Defensive Midfield", "x": 32, "y": 40},
+        {"role": "Left Center Midfield", "x": 40, "y": 30},
+        {"role": "Right Center Midfield", "x": 40, "y": 50},
+        {"role": "Left Attacking Midfield", "x": 50, "y": 30},
+        {"role": "Right Attacking Midfield", "x": 50, "y": 50},
+        {"role": "Center Forward", "x": 60, "y": 40}
+    ],
+    "433": [
+        {"role": "Goalkeeper", "x": 5, "y": 40},
+        {"role": "Left Back", "x": 20, "y": 15},
+        {"role": "Left Center Back", "x": 20, "y": 30},
+        {"role": "Right Center Back", "x": 20, "y": 50},
+        {"role": "Right Back", "x": 20, "y": 65},
+        {"role": "Left Center Midfield", "x": 35, "y": 25},
+        {"role": "Center Midfield", "x": 35, "y": 40},
+        {"role": "Right Center Midfield", "x": 35, "y": 55},
+        {"role": "Left Wing", "x": 50, "y": 15},
+        {"role": "Right Wing", "x": 50, "y": 65},
+        {"role": "Center Forward", "x": 60, "y": 40}
+    ],
+    "4411": [
+        {"role": "Goalkeeper", "x": 5, "y": 40},
+        {"role": "Left Back", "x": 20, "y": 15},
+        {"role": "Left Center Back", "x": 20, "y": 30},
+        {"role": "Right Center Back", "x": 20, "y": 50},
+        {"role": "Right Back", "x": 20, "y": 65},
+        {"role": "Left Midfield", "x": 35, "y": 15},
+        {"role": "Left Center Midfield", "x": 35, "y": 30},
+        {"role": "Right Center Midfield", "x": 35, "y": 50},
+        {"role": "Right Midfield", "x": 35, "y": 65},
+        {"role": "Center Attacking Midfield", "x": 50, "y": 40},
+        {"role": "Center Forward", "x": 60, "y": 40}
+    ],
+    "442": [
+        {"role": "Goalkeeper", "x": 5, "y": 40},
+        {"role": "Left Back", "x": 20, "y": 15},
+        {"role": "Left Center Back", "x": 20, "y": 30},
+        {"role": "Right Center Back", "x": 20, "y": 50},
+        {"role": "Right Back", "x": 20, "y": 65},
+        {"role": "Left Midfield", "x": 35, "y": 15},
+        {"role": "Left Center Midfield", "x": 35, "y": 30},
+        {"role": "Right Center Midfield", "x": 35, "y": 50},
+        {"role": "Right Midfield", "x": 35, "y": 65},
+        {"role": "Left Center Forward", "x": 60, "y": 35},
+        {"role": "Right Center Forward", "x": 60, "y": 45}
+    ],
+    "451": [
+        {"role": "Goalkeeper", "x": 5, "y": 40},
+        {"role": "Left Back", "x": 20, "y": 15},
+        {"role": "Left Center Back", "x": 20, "y": 30},
+        {"role": "Right Center Back", "x": 20, "y": 50},
+        {"role": "Right Back", "x": 20, "y": 65},
+        {"role": "Left Midfield", "x": 35, "y": 15},
+        {"role": "Left Center Midfield", "x": 35, "y": 30},
+        {"role": "Center Midfield", "x": 35, "y": 40},
+        {"role": "Right Center Midfield", "x": 35, "y": 50},
+        {"role": "Right Midfield", "x": 35, "y": 65},
+        {"role": "Center Forward", "x": 60, "y": 40}
+    ],
+    "5221": [
+        {"role": "Goalkeeper", "x": 5, "y": 40},
+        {"role": "Left Back", "x": 20, "y": 15},
+        {"role": "Left Center Back", "x": 20, "y": 30},
+        {"role": "Right Center Back", "x": 20, "y": 50},
+        {"role": "Right Back", "x": 20, "y": 65},
+        {"role": "Left Center Midfield", "x": 35, "y": 30},
+        {"role": "Right Center Midfield", "x": 35, "y": 50},
+        {"role": "Left Wing", "x": 50, "y": 20},
+        {"role": "Right Wing", "x": 50, "y": 60},
+        {"role": "Center Forward", "x": 60, "y": 40},
+        {"role": "Center Midfield", "x": 35, "y": 40}
+    ],
+    "532": [
+        {"role": "Goalkeeper", "x": 5, "y": 40},
+        {"role": "Left Wing Back", "x": 20, "y": 10},
+        {"role": "Left Center Back", "x": 20, "y": 25},
+        {"role": "Center Back", "x": 20, "y": 40},
+        {"role": "Right Center Back", "x": 20, "y": 55},
+        {"role": "Right Wing Back", "x": 20, "y": 70},
+        {"role": "Left Center Midfield", "x": 35, "y": 30},
+        {"role": "Center Midfield", "x": 35, "y": 40},
+        {"role": "Right Center Midfield", "x": 35, "y": 50},
+        {"role": "Left Center Forward", "x": 60, "y": 35},
+        {"role": "Right Center Forward", "x": 60, "y": 45}
+    ],
+    "541": [
+        {"role": "Goalkeeper", "x": 5, "y": 40},
+        {"role": "Left Wing Back", "x": 20, "y": 10},
+        {"role": "Left Center Back", "x": 20, "y": 25},
+        {"role": "Center Back", "x": 20, "y": 40},
+        {"role": "Right Center Back", "x": 20, "y": 55},
+        {"role": "Right Wing Back", "x": 20, "y": 70},
+        {"role": "Left Center Midfield", "x": 35, "y": 30},
+        {"role": "Center Midfield", "x": 35, "y": 40},
+        {"role": "Right Center Midfield", "x": 35, "y": 50},
+        {"role": "Left Attacking Midfield", "x": 50, "y": 30},
+        {"role": "Right Attacking Midfield", "x": 50, "y": 50}
+    ]
+}
+
